c9fe7f01312800b416c207d30912d414c10421c5
DEVELOD WITH SPIDY <avinashraz19818@gmail.com>
2026-04-22 11:28:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
+# config.py
+
 TOKEN = "8565961811:AAGrhql1L3Na4M9tz5BMhI5qitA7vpGzQ-w"
 APIID = 10248430
 APIHASH = "42396a6ff14a569b9d5931643897d0d"
 
-# Set your Telegram user ID here to get join notifications
-# Leave as None to disable notifications
-NOTIFY_USER_ID = 8015937475  # e.g., 123456789
+# Owner ID (aapka Telegram user ID)
+OWNER_ID = 8015937475
+
+# Other configurations
+SESSION_DIR = "sessions"
+AUDIO_DIR = "audios"
+DATA_FILE = "data.json"
+ENCRYPTION_KEY_FILE = "encryption.key"
+DEFAULT_GAP_SECONDS = 5
+DEFAULT_BREAK_SECONDS = 60
+LIVESTREAM_CHECK_INTERVAL = 10
