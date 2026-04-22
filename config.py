eda3cc94019c3addafd3df354b7bc913c71da554
DEVELOD WITH SPIDY <avinashraz19818@gmail.com>
2026-04-22 11:12:32 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = "8565961811:AAGrhql1L3Na4M9tz5BMhI5qitA7vpGzQ-w"
+APIID = 10248430
+APIHASH = "42396a6ff14a569b9d5931643897d0d"
+
+# Set your Telegram user ID here to get join notifications
+# Leave as None to disable notifications
+NOTIFY_USER_ID = 8015937475  # e.g., 123456789
